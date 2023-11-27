c44f34407de89e997bc45a6ed6e2be566977ca9c
minirius <marius.nerantzakis@gmail.com>
2023-11-27 21:32:19 +0100

    commit

--- python/Chapitre 5/projet.py
+++ python/Chapitre 5/projet.py
@@ -8,10 +8,12 @@ import turtle
 import random
 import math
 
+COLORS = ["RoyalBlue", "burlywood1", "Cornflowerblue", "DarkOrange", "DarkSeaGreen1", "lightCyan", "firebrick1", "slateBlue2"]
+
 def couleur_aleatoire():
     liste_couleur = ["RoyalBlue", "grey20", "green2", "red1", "BlueViolet"]
     return random.choice(liste_couleur)
-def trait(x1, y1, x2, y2):
+def trait(x1, y1, x2, y2, pensize=1):
     turtle.goto(x1, y1)
     turtle.pendown()
     turtle.goto(x2, y2)
@@ -38,14 +40,14 @@ def rectangle(x, y, w, h, color="white", pensize=1):
     turtle.penup()
 
 def sol(y_sol):
-    trait(-300, y_sol, 300, y_sol)
+    trait(-400, y_sol, 400, y_sol, pensize=3)
 
 def toit_plat(x, y_sol, niveau, color="black"):
-    rectangle(x-80, 60*niveau, w=160, h=10, color=color)
+    rectangle(x-80, y_sol+60*niveau, w=160, h=10, color=color)
 
 def toit_triangle(x, y_sol, niveau, color="black"):
     turtle.fillcolor(color)
-    turtle.goto(x-80, 60*niveau)
+    turtle.goto(x-80, y_sol+60*niveau)
     turtle.pendown()
     turtle.begin_fill()
 
@@ -67,16 +69,16 @@ def toit(x, y_sol, niveau,):
         toit_triangle(x, y_sol, niveau)
 
 def fenetre(x,y):
-    rectangle(x-15, y+20, w=30, h=30, color="grey")
+    rectangle(x-15, y+20, w=30, h=30, color="azure")
 
 def fenetre_balcon(x, y):
-    rectangle(x-15, y, w=30, h=50, color="grey")
-    rectangle(x-18, y, w=18*2, h=30, color="transparent", pensize=2)
+    rectangle(x-15, y, w=30, h=50, color="azure")
+    rectangle(x-18, y, w=18*2, h=25, color="transparent", pensize=2)
     for i in range(0, 36, 6):
-        rectangle(x-18+i, y, w=6, h=30, color="transparent", pensize=2)
+        rectangle(x-18+i, y, w=6, h=25, color="transparent", pensize=2)
 
 def porte(x, y, couleur="white"):
-    if(random.choice([1, 0]) == 1):
+    if(random.choice([1, 1]) == 1):
         rectangle(x-15, y, w=30, h=55, color=couleur)
     else:
         rectangle(x-15, y, w=30, h=40, color=couleur)
@@ -94,36 +96,50 @@ def facade(x, y_sol, couleur, niveau):
     None
 
 def rdc(x, y_sol, c_facade, c_porte):
-    rectangle(x-70, y_sol, w=150, h=60, color=c_facade)
-    place = random.shuffle([0, 0, 1])
-    i = 1
-    for element in place:
-        if element == 0:
-            fenetre(i*(140/4), y_sol)
-        else:
-            porte(i*(140/4), y_sol, couleur=c_porte)
+    rectangle(x-70, y_sol, w=140, h=60, color=c_facade)
+    place=[0, 0, 1]
+    random.shuffle(place)
+    for i, element in enumerate(place):
         i+=1
+        if element == 0:
+            fenetre((x-70)+i*35, y_sol)
+        else:
+            porte((x-70)+i*35, y_sol, couleur=c_porte)
 
 def etage(x, y_sol, couleur, niveau):
-    None
+    rectangle(x-70, y_sol+niveau*60, w=140, h=60, color=couleur)
+    for i in range(1,4):
+        element = random.randint(0, 1)
+        if element == 0:
+            fenetre((x-70)+i*35, y_sol+niveau*60)
+        else:
+            fenetre_balcon((x-70)+i*35, y_sol+niveau*60)
 
 def immeuble(x, y_sol):
-    None
+    global COLORS
+    color = random.choice(COLORS)
+    COLORS.remove(color)
+    color2 = random.choice(COLORS)
+    COLORS.remove(color2)
+    hauteur = random.randint(3,4)
+
+    rdc(x, y_sol, color, color2)
+    for i in range(1, hauteur):
+        etage(x, y_sol, color,i)
+    toit(x, y_sol, hauteur)
 
 def main():
     turtle.penup()
     turtle.hideturtle()
-    turtle.speed(0)
+    turtle.tracer(0)
+    turtle.Screen().setup(900, 600)
+    SOL = -150
+    sol(SOL)
+    for i in range(0, 4):
+        immeuble(i*200 - 300, SOL)
 
-    '''rectangle(0, 0, 140, 60, 'blue')
-    fenetre(35, 0)
-    #fenetre_balcon(70,0)
-    porte(70, 0, 'red')
-    fenetre(105, 0)
-    toit(70, 0, 1)'''
+    # On ferme la fenêtre s'il y a un clique gauche
+    turtle.exitonclick()
 
-    rdc(0, 0, "white", "yellow")
-
-    turtle.mainloop()
-
-main()
+if __name__ == '__main__':
+    main()
