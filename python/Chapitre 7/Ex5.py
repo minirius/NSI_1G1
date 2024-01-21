4ee4c496f4ec8e26734d02ea8fe59c2fdb511c07
minirius <marius.nerantzakis@gmail.com>
2024-01-21 09:20:49 +0100

    ajout du dictionnaire Ex5

--- python/Chapitre 7/Ex5.py
+++ python/Chapitre 7/Ex5.py
@@ -1,9 +1,10 @@
 t1 = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 t2 = ['janvier', 'fevrier', "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"]
 t3 = []
+d = {e:t1[i] for i, e in enumerate(t2)}
 
 for i, e in enumerate(t2):
     t3.append(e)
     t3.append(t1[i])
 
-print(t3)
+print(t3, d)
