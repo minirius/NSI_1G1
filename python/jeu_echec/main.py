5be4e8af7ba455143f7c30c87954f6c8ae985a12
minirius <marius.nerantzakis@gmail.com>
2024-02-02 06:57:36 +0100

    tour par tour

--- python/jeu_echec/main.py
+++ python/jeu_echec/main.py
@@ -1,25 +1,23 @@
 import time
 import tkinter as tk
+from tkinter import simpledialog
 
 SIZE=80
 TABLEAU = [["" for j in range(8)] for i in range(8)]
 LISTE_PIECE = {
-    "pion_blanc" : [
-        "caseX;caseY-1",
-    ],
-    "tour_blanc" : [
+    "tour" : [
         [f"caseX-{i+1};caseY" for i in range(8)],
         [f"caseX+{i+1};caseY" for i in range(8)],
         [f"caseX;caseY-{i+1}" for i in range(8)],
         [f"caseX;caseY+{i+1}" for i in range(8)],
     ],
-    "fou_blanc" : [
+    "fou" : [
         [f"caseX-{i+1};caseY-{i+1}" for i in range(8)],
         [f"caseX-{i+1};caseY+{i+1}" for i in range(8)],
         [f"caseX+{i+1};caseY-{i+1}" for i in range(8)],
         [f"caseX+{i+1};caseY+{i+1}" for i in range(8)],
     ],
-    "cavalier_blanc" : [
+    "cavalier" : [
         "caseX-2;caseY-1",
         "caseX-2;caseY+1",
         "caseX+2;caseY-1",
@@ -29,7 +27,7 @@ LISTE_PIECE = {
         "caseX-1;caseY+2",
         "caseX+1;caseY+2",
     ],
-    "reine_blanc" : [
+    "reine" : [
         [f"caseX-{i+1};caseY-{i+1}" for i in range(8)],
         [f"caseX-{i+1};caseY+{i+1}" for i in range(8)],
         [f"caseX+{i+1};caseY-{i+1}" for i in range(8)],
@@ -39,7 +37,7 @@ LISTE_PIECE = {
         [f"caseX;caseY-{i+1}" for i in range(8)],
         [f"caseX;caseY+{i+1}" for i in range(8)],
     ],
-    "roi_blanc" : [
+    "roi" : [
         "caseX-1;caseY-1",
         "caseX-1;caseY+1",
         "caseX+1;caseY-1",
@@ -52,10 +50,6 @@ LISTE_PIECE = {
 }
 
 def map(value , start1, end1, scale1, scale2):
-    #
-    # value         scaleValue
-    # val2 - val1   scale2 - scale1
-    #
     return int(((value*(scale2 - scale1))/(end1 - start1)) + scale1)
 
 class Game(tk.Tk):
@@ -76,6 +70,89 @@ class Game(tk.Tk):
         self.setupTableau()
         self.drawScreen()
     
+    def placeRed(self, x: int, y:int) -> None:
+        self.canvas.create_rectangle((x*SIZE, y*SIZE), ((x+1)*SIZE, (y+1)*SIZE), outline="red", width=4)
+        self.possibilities.append((x, y))
+    
+    def placeBlue(self, x: int, y:int) -> None:
+        self.canvas.create_oval((x*SIZE+25, y*SIZE+25),((x+1)*SIZE-25, (y+1)*SIZE-25), fill="cadetblue2", width=0)
+        self.possibilities.append((x, y))
+    
+    def placePoint(self, x: int, y:int) -> None:
+        if(self.tour == 0):
+            if("noir" in TABLEAU[y][x]):
+                self.placeRed(x, y)
+            elif TABLEAU[y][x] == "":
+                self.placeBlue(x, y)
+        elif(self.tour == 7):
+            if("blanc" in TABLEAU[y][x]):
+                self.placeRed(x, y)
+            elif TABLEAU[y][x] == "":
+                self.placeBlue(x, y)
+            
+    def debug(self, table):
+        for i in table:
+            for j in i:
+                print(str(j).rjust(15), end="")
+            print()
+            
+    def flip_tableau(self):
+        global TABLEAU
+        TEMP_TABLEAU = [["" for i in range(8)] for i in range(8)] 
+        for x, e in enumerate(TABLEAU):
+            for y, E in  enumerate(e):
+                TEMP_TABLEAU[7-x][7-y] = E
+        '''self.debug(TABLEAU)
+        self.debug(TEMP_TABLEAU)'''
+        TABLEAU = TEMP_TABLEAU.copy()
+        self.tour = abs(self.tour - 7)
+        self.drawScreen()
+    
+    def check_for_check(self):
+        TEMP_TABLEAU = [e for e in TABLEAU if e!=""]
+        for x, e in enumerate(TABLEAU):
+            for y, E in enumerate(e):
+                caseY, caseX = y, x
+                if(self.tour == 0 and "noir" in TABLEAU[caseY][caseX]):
+                    continue
+                elif(self.tour == 7 and "blanc" in TABLEAU[caseY][caseX]):
+                    continue
+                elif(TABLEAU[caseY][caseX] == ""):
+                    continue
+                if("pion" in TABLEAU[caseY][caseX]):
+                    try:
+                        if("roi" in TABLEAU[caseY-1][caseX-1] and self.tour == 0):
+                            return True
+                        elif("roi" in TABLEAU[caseY-1][caseX+1] and self.tour == 0):
+                            return True
+                        elif("roi" in TABLEAU[caseY+1][caseX-1] and self.tour == 7):
+                            return True
+                        elif("roi" in TABLEAU[caseY+1][caseX+1] and self.tour == 7):
+                            return True
+                        else:
+                            continue
+                    except:
+                        print("Out Range")
+                for move in LISTE_PIECE[TABLEAU[caseY][caseX].split("_")[0]]:
+                        if(type(move) == str):
+                            newCaseX = eval(move.split(";")[0])
+                            newCaseY = eval(move.split(";")[1])
+                            if(0 > newCaseX or newCaseX > 7 or 0 > newCaseY or newCaseY > 7):
+                                continue
+                            if("roi" in TABLEAU[newCaseY][newCaseX]):
+                                return True
+                        else:
+                            for sub_move in move:
+                                newCaseX = eval(sub_move.split(";")[0])
+                                newCaseY = eval(sub_move.split(";")[1])
+                                if(0 > newCaseX or newCaseX > 7 or 0 > newCaseY or newCaseY > 7):
+                                    continue
+                                if(TABLEAU[newCaseY][newCaseX] == ""):
+                                    continue
+                                elif("roi" in TABLEAU[newCaseY][newCaseX]):
+                                    return True
+                                break
+        
     def clickHandler(self, event):
         self.drawScreen()
         x, y = event.x, event.y
@@ -87,48 +164,40 @@ class Game(tk.Tk):
             self.possibilities = []
             self.oldPos = (caseX, caseY)
             self.canvas.create_rectangle((caseX*SIZE+3, caseY*SIZE+3), ((caseX+1)*SIZE-3, (caseY+1)*SIZE-3), outline="chartreuse1", width=6)
-            if(TABLEAU[caseY][caseX]=="pion_blanc"):
+            if("pion" in TABLEAU[caseY][caseX]):
                 newCaseX, newCaseY = caseX, caseY-1
-                if("noir" in TABLEAU[newCaseY][newCaseX]):
-                    self.canvas.create_rectangle((newCaseX*SIZE, newCaseY*SIZE), ((newCaseX+1)*SIZE, (newCaseY+1)*SIZE), outline="red", width=4)
-                    self.possibilities.append((newCaseX, newCaseY))
-                elif TABLEAU[newCaseY][newCaseX] == "":
-                    self.canvas.create_oval((newCaseX*SIZE+25, newCaseY*SIZE+25),((newCaseX+1)*SIZE-25, (newCaseY+1)*SIZE-25), fill="cadetblue2", width=0)
-                    self.possibilities.append((newCaseX, newCaseY))
+                self.placePoint(newCaseX, newCaseY)
 
                 if(caseY == 6 and TABLEAU[newCaseY][newCaseX] == ""):
                     newCaseX, newCaseY = caseX, caseY-2
-                    if("noir" in TABLEAU[newCaseY][newCaseX]):
-                        self.canvas.create_rectangle((newCaseX*SIZE, newCaseY*SIZE), ((newCaseX+1)*SIZE, (newCaseY+1)*SIZE), outline="red", width=4)
-                        self.possibilities.append((newCaseX, newCaseY))
-                    elif TABLEAU[newCaseY][newCaseX] == "":
-                        self.canvas.create_oval((newCaseX*SIZE+25, newCaseY*SIZE+25),((newCaseX+1)*SIZE-25, (newCaseY+1)*SIZE-25), fill="cadetblue2", width=0)
-                        self.possibilities.append((newCaseX, newCaseY))
+                    self.placePoint(newCaseX, newCaseY)
 
                 newCaseX, newCaseY = caseX-1, caseY-1
                 if not(0 > newCaseX or newCaseX > 7 or 0 > newCaseY or newCaseY > 7):
-                    if("noir" in TABLEAU[newCaseY][newCaseX]):
-                        self.canvas.create_rectangle((newCaseX*SIZE, newCaseY*SIZE), ((newCaseX+1)*SIZE, (newCaseY+1)*SIZE), outline="red", width=4)
-                        self.possibilities.append((newCaseX, newCaseY))
+                    if(self.tour == 0):
+                        if("noir" in TABLEAU[newCaseY][newCaseX]):
+                            self.placeRed(caseX, caseY)
+                    else:
+                        if("blanc" in TABLEAU[newCaseY][newCaseX]):
+                            self.placeRed(caseX, caseY)
                 
                 newCaseX, newCaseY = caseX+1, caseY-1
                 if not (0 > newCaseX or newCaseX > 7 or 0 > newCaseY or newCaseY > 7):
-                    if("noir" in TABLEAU[newCaseY][newCaseX]):
-                        self.canvas.create_rectangle((newCaseX*SIZE, newCaseY*SIZE), ((newCaseX+1)*SIZE, (newCaseY+1)*SIZE), outline="red", width=4)
-                        self.possibilities.append((newCaseX, newCaseY))
-            elif(TABLEAU[caseY][caseX] and TABLEAU[caseY][caseX] in LISTE_PIECE):
-                for move in LISTE_PIECE[TABLEAU[caseY][caseX]]:
+                    if(self.tour == 0):
+                        if("noir" in TABLEAU[newCaseY][newCaseX]):
+                            self.placeRed(caseX, caseY)
+                    else:
+                        if("blanc" in TABLEAU[newCaseY][newCaseX]):
+                            self.placeRed(caseX, caseY)
+
+            elif(TABLEAU[caseY][caseX] and TABLEAU[caseY][caseX].split("_")[0] in LISTE_PIECE):
+                for move in LISTE_PIECE[TABLEAU[caseY][caseX].split("_")[0]]:
                     if(type(move) == str):
                         newCaseX = eval(move.split(";")[0])
                         newCaseY = eval(move.split(";")[1])
                         if(0 > newCaseX or newCaseX > 7 or 0 > newCaseY or newCaseY > 7):
                             continue
-                        if(TABLEAU[newCaseY][newCaseX] == ""):
-                            self.canvas.create_oval((newCaseX*SIZE+25, newCaseY*SIZE+25),((newCaseX+1)*SIZE-25, (newCaseY+1)*SIZE-25), fill="cadetblue2", width=0)
-                            self.possibilities.append((newCaseX, newCaseY))
-                        elif("noir" in TABLEAU[newCaseY][newCaseX]):
-                            self.canvas.create_rectangle((newCaseX*SIZE, newCaseY*SIZE), ((newCaseX+1)*SIZE, (newCaseY+1)*SIZE), outline="red", width=4)
-                            self.possibilities.append((newCaseX, newCaseY))
+                        self.placePoint(newCaseX, newCaseY)
                     else:
                         for sub_move in move:
                             newCaseX = eval(sub_move.split(";")[0])
@@ -136,22 +205,24 @@ class Game(tk.Tk):
                             if(0 > newCaseX or newCaseX > 7 or 0 > newCaseY or newCaseY > 7):
                                 continue
                             if(TABLEAU[newCaseY][newCaseX] == ""):
-                                self.canvas.create_oval((newCaseX*SIZE+25, newCaseY*SIZE+25),((newCaseX+1)*SIZE-25, (newCaseY+1)*SIZE-25), fill="cadetblue2", width=0)
-                                self.possibilities.append((newCaseX, newCaseY))
-                            elif("noir" in TABLEAU[newCaseY][newCaseX]):
-                                self.canvas.create_rectangle((newCaseX*SIZE, newCaseY*SIZE), ((newCaseX+1)*SIZE, (newCaseY+1)*SIZE), outline="red", width=4)
-                                self.possibilities.append((newCaseX, newCaseY))
-                                break
-                            else:
-                                break
+                                self.placeBlue(newCaseX, newCaseY)
+                                continue
+                            if(self.tour == 0):
+                                if("noir" in TABLEAU[newCaseY][newCaseX]):
+                                    self.placeRed(newCaseX, newCaseY)
+                            elif(self.tour == 7):
+                                if("blanc" in TABLEAU[newCaseY][newCaseX]):
+                                    self.placeRed(newCaseX, newCaseY)
+                            break
 
         else:
             if((caseX, caseY) in self.possibilities):
                 print("Moved !")
                 TABLEAU[caseY][caseX] = TABLEAU[self.oldPos[1]][self.oldPos[0]]
                 TABLEAU[self.oldPos[1]][self.oldPos[0]] = ""
-                #self.tour = abs(self.tour - 7)
                 self.drawScreen()
+                print(self.check_for_check())
+                self.after(500, self.flip_tableau)
 
             self.click_state = "unselected"
             self.possibilities = []
@@ -161,10 +232,20 @@ class Game(tk.Tk):
         self.canvas.delete("all")
         self.images = []
         for i in range(64):
-            X = abs(self.tour - i%8)
-            Y = abs(self.tour - i//8)
-            print(self.tour, X, Y, i%8, i//8)
-            color = "coral4" if (X+Y)%2 else "aliceblue"
+            X = i%8
+            Y = i//8
+            match THEME:
+                case 1:
+                    color = "coral4" if (X+Y)%2 else "aliceblue"
+                case 2:
+                    color = "burlywood4" if (X+Y)%2 else "burlywood1"
+                case 3:
+                    color = "lightcyan4" if (X+Y)%2 else "linen"
+                case 4:
+                    color = "darkgoldenrod2" if (X+Y)%2 else "gray98"
+                case 5:
+                    color = "forestgreen" if (X+Y)%2 else "darkolivegreen2"
+
             self.canvas.create_rectangle(((X)*SIZE, (Y)*SIZE), ((X+1)*SIZE, (Y+1)*SIZE), fill=color)
             if(TABLEAU[i//8][i%8] != ""):
                 self.images.append(tk.PhotoImage(file=f"python/jeu_echec/{TABLEAU[i//8][i%8]}.png"))
@@ -178,5 +259,9 @@ class Game(tk.Tk):
         print(TABLEAU)
 
 if __name__ == "__main__":
+    #IP_ADRR = simpledialog.askstring("IP Adresse", "Veuillez entrer l'IP de votre adversaire")
+    THEME = int(simpledialog.askstring("Thème", "Veuillez un nombre entre 1 et 5"))
+    if THEME > 6 or THEME < 1:
+        THEME = 1
     app = Game()
     app.mainloop()
